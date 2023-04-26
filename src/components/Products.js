628840c87428490ab03ecf2ff1feffece539cce1
Dotzooo <projectnba77@gmail.com>
2023-04-26 10:56:10 +0800

    feat: fake product data

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,26 +1,31 @@
+import ProductsData from "../assets/productsData";
+
 export default function Products() {
-    return (
-        <div className="col">
+  return (
+    <div className="row row-cols-3 g-3">
+      {ProductsData.map((product) => {
+        return (
+          <div className="col" key={product.id}>
             <div className="card">
-                <img
-                    src="https://images.unsplash.com/photo-1682347546932-f397ccf63940?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-                    className="card-img-top"
-                    alt="..."
-                />
-                <div className="card-body">
-                    <h6 className="card-title">
-                        Card title
-                        <span className="float-end">$250</span>
-                    </h6>
-                    <p className="card-text"></p>
-                    <button
-                        type="button"
-                        className="btn btn-outline-primary w-100"
-                    >
-                        Go somewhere
-                    </button>
-                </div>
+              <img
+                src={product.img}
+                className="card-img-top"
+                alt={product.title}
+              />
+              <div className="card-body">
+                <h6 className="card-title">
+                  {product.title}
+                  <span className="float-end">NT$ {product.price}</span>
+                </h6>
+                <p className="card-text"></p>
+                <button type="button" className="btn btn-outline-primary w-100">
+                  加入購物車
+                </button>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        );
+      })}
+    </div>
+  );
 }
